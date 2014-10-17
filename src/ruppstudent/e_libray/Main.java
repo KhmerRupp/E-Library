c646dd561454466f1058fce644a178de2aaeca34
sreangrathanak <sreangrathanak@yahoo.com>
2014-10-18 00:02:45 +0700

    First Init

---
+++ src/ruppstudent/e_libray/Main.java
@@ -0,0 +1,14 @@
+package ruppstudent.e_libray;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+}
